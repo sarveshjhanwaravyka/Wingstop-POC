9858dfe2f7c9a6513b84a4c9862a8ddb756e1443
Sarvesh Jhanwar <sarvesh.jhanwar@vivaopss.onmicrosoft.com>
2026-02-20 14:19:52 +0530

    Added Files

---
+++ app_morning/morning.py
@@ -0,0 +1,9 @@
+import time
+
+def main():
+    timestamp = time.strftime("%H:%M:%S")
+    print(f"[{timestamp}] Good Morning Wingstop!")
+    print("Prepping the lemon pepper and original hot...")
+
+if __name__ == "__main__":
+    main()
